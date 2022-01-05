112deb3bc4773e5fe977c6393c23edb30be33744
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-01-04 23:40:35 -0300

    docs: create first part

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Platform } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar 
+        backgroundColor="#FFF" 
+        translucent={false} 
+      />
+
+      <Image 
+        source={require('./src/assets/logo.png')} 
+        style={styles.logo} 
+      />
+    
+      <TextInput
+        placeholder="Celular, username ou email"
+        style={styles.input}
+      />
+
+      <TextInput
+        placeholder="Senha"
+        style={styles.input}
+      />
+
+      <View style={styles.forgotContainer}>
+        <TouchableOpacity>
+          <Text style={styles.forgotText}>Esqueceu sua senhas?</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginText}>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#FFF',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  logo: {
+    marginTop: Platform.OS === 'android' ? '13%' : '20%',
+    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
+  },
+  input: {
+    width: '85%',
+    height: 42,
+    backgroundColor: '#F4F3F3',
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 5,
+    borderColor: '#E0E0E0'
+  },
+  forgotContainer: {
+    width: '90%',
+    alignItems: 'flex-end',
+  },
+  forgotText: {
+    color: '#399fff',
+  },
+  loginButton: {
+    marginTop: '5%',
+    backgroundColor: '#399fff',
+    width: '85%',
+    height: 42,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  loginText: {
+    color: '#FFF',
+    fontSize: 16,
+
+  }
 });
